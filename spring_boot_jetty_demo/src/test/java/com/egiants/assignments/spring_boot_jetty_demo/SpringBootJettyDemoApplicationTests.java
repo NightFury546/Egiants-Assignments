ab1ce10f2a14b135d5508b773fcb4d3fca28af05
NightFury546 <ymanojkumar.se@gmail.com>
2019-06-12 00:30:38 -0400

    spring boot with jetty commit

---
+++ spring_boot_jetty_demo/src/test/java/com/egiants/assignments/spring_boot_jetty_demo/SpringBootJettyDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.egiants.assignments.spring_boot_jetty_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJettyDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
